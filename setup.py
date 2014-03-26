85106ef98edd95d657ac0ed7b6ae9ee366c1800e
Uche Ogbuji <uche@ogbuji.net>
2014-03-25 19:32:25 -0600

    Fast forward many changes & additions

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from distutils.core import setup
 
 setup(name = "datachef",
       #version = __version__,
-      version = "0.2.0",
+      version = "0.2.2",
       description="Data chef recipes, a grab bag for dealing with all sorts of data",
       author='Uche Ogbuji',
       author_email='uche@ogbuji.net',
       url='http://uche.ogbuji.net',
       package_dir={'datachef': 'lib'},
-      packages=['datachef', 'datachef.exhibit', 'datachef.geo', 'datachef.thirdparty'],
-      scripts=['script/exhibit_agg',],
+      packages=['datachef', 'datachef.exhibit', 'datachef.geo', 'datachef.tabular', 'datachef.ids', 'datachef.thirdparty'],
+      scripts=['cmdline/exhibit_agg',],
       #package_data={'akara': ["akara.conf"]},
       )
